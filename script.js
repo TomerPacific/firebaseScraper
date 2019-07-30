87aa1ed8e77761c1fce90eeca391fc0c01a46e5b
unknown <tomerpacific@gmail.com>
2019-07-30 21:44:05 +0300

    FirebaseScraper adding CORS header to request

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const url = "https://firebase-scraper.herokuapp.com/firebase";
 
 
 var xmlHttp = new XMLHttpRequest();
+xmlHttp.withCredentials = true;
+xmlHttp.setRequestHeader("Content-Type","application/json");
+xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+
 xmlHttp.onreadystatechange = function() { 
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
         console.log(xmlHttp.responseText);
