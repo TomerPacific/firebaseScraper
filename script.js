ac2cd98723b834c5e8a906738d9c3127d15f589f
unknown <tomerpacific@gmail.com>
2019-07-30 21:58:15 +0300

    FirebaseScraper removing header lines

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const url = "https://firebase-scraper.herokuapp.com/firebase";
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.open("GET", url, true);
 xmlHttp.withCredentials = true;
-xmlHttp.setRequestHeader("Content-Type","application/json");
-xmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*");
 
 xmlHttp.onreadystatechange = function() { 
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
