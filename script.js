434385bdb3819c8f0eeb438b219580ae62006820
unknown <tomerpacific@gmail.com>
2019-07-31 23:19:40 +0300

    FirebaseScraper logic to present response from server

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+let productsList = document.getElementById("products");
+
+
 const url = "https://firebase-scraper.herokuapp.com/firebase";
-
-
 
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.open("GET", url, true);
 xmlHttp.setRequestHeader("Content-Type", "application/json");
 
 xmlHttp.onload = function() {
-	console.log('onload');
 	var responseText = xmlHttp.responseText;
-	console.log(responseText);
+	let val = responseText.message;
+	let liElem = document.createElement("li");
+	liElem.innerHTML = val;
+	productsList.appendChild(liElem);
 };
 
 xmlHttp.onerror = function() {
